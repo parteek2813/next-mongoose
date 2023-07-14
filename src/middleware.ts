a03981be916ad6a6da38c0c16b2ca1813410f760
parteek kumar <digitalparteek@gmail.com>
2023-07-14 18:34:16 +0530

    added protected routes

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+
+  const isPublicPath = path === "/login" || path === "/signup";
+  const token = request.cookies.get("token")?.value || "";
+
+  // if the path is public and token is there, then redirect
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/", request.nextUrl));
+  }
+
+  // if the path is not public and token is also not there, then redirect to login page
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/", "/profile", "/login", "/signup"],
+};
