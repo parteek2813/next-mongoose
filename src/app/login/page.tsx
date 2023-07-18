88bdc158406b33234209a1fccc3bb35a4892be24
parteek kumar <digitalparteek@gmail.com>
2023-07-18 13:08:18 +0530

    improved login ui and minor fixes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -15,6 +15,7 @@ function LoginPage() {
 
   const [buttonDisabled, setButtonDisabled] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
+  const [buttonText, setButtonText] = React.useState("Login");
 
   // useEffect on user
   useEffect(() => {
@@ -29,49 +30,63 @@ function LoginPage() {
   const onLogin = async () => {
     try {
       setLoading(true);
+      setButtonText("Authenticating...");
       const response = await axios.post("/api/users/login", user);
-      console.log("Login success", response.data);
       toast.success("Login success");
-      router.push("/profile"); // after login success, go to profile page
+      setButtonText("Login success");
+      setTimeout(() => {
+        router.push("/profile"); // after login success, go to profile page
+      }, 100);
     } catch (error: any) {
       console.log("Login failed", error.message);
-      toast.error(error.message);
-    } finally {
+      setButtonText("Login");
       setLoading(false);
+      toast.error(error.message);
     }
   };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="">{loading ? "Processing" : "Login"}</h1>
+      <h1 className="text-4xl sm:text-6xl text-center font-bold">Login</h1>
+      <hr className="w-2/3 sm:w-1/3 my-8 border-gray-300" />
+      {/* <h1 className="">{loading ? "Processing" : "Login"}</h1> */}
 
-      <label htmlFor="email">email</label>
-      <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
-        id="email"
-        type="text"
-        value={user.email}
-        onChange={(e) => setUser({ ...user, email: e.target.value })} // adding the email on the fly and this is set onto the user
-        placeholder="email"
-      ></input>
-      <label htmlFor="password">password</label>
-      <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
-        id="password"
-        type="password"
-        value={user.password}
-        onChange={(e) => setUser({ ...user, password: e.target.value })}
-        placeholder="password"
-      ></input>
-      <button
-        onClick={onLogin}
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 "
-      >
-        {buttonDisabled ? "No Login" : "Login"}
-      </button>
-      <Link href="/signup">Visit Signup page </Link>
-      <Link className="py-2" href="/resetpassword">
-        Forgot password?
-      </Link>
+      <div className="w-5/6 sm:w-1/2">
+        <input
+          className="w-full p-4 mb-4 rounded-lg border border-gray-300 focus:outline-none focus:border-gray-600 text-black"
+          id="email"
+          type="text"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          placeholder="Email"
+        />
+        <label htmlFor="password">password</label>
+        <input
+          className="w-full p-4 mb-4 rounded-lg border border-gray-300 focus:outline-none focus:border-gray-600 text-black"
+          id="password"
+          type="password"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          placeholder="Password"
+        />
+        <div className="flex flex-col sm:flex-row justify-between mb-4">
+          <Link
+            href="/resetpassword"
+            className="text-white-500 hover:underline"
+          >
+            Forgot password?
+          </Link>
+          <Link href="/signup" className="text-white-500 hover:underline">
+            {"Don't have an account? Sign up"}
+          </Link>
+        </div>
+        <button
+          onClick={onLogin}
+          className="w-full font-bold rounded-lg bg-green-500 hover:bg-green-600 text-white py-4 transition-colors duration-300"
+          disabled={buttonDisabled || loading}
+        >
+          {buttonText}
+        </button>
+      </div>
     </div>
   );
 }
